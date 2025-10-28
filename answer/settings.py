64c91d52e900a2e47304f75943b0c1c97313e62c
Anton Pembek <98619025+Antondfger@users.noreply.github.com>
2025-10-28 21:58:44 +0300

    Update settings.py

--- answer/settings.py
+++ answer/settings.py
@@ -27,9 +27,9 @@ class Settings(BaseSettings):
         
     QDRANT_API_KEY: str
         
-    SERVICE_ACCOUNT_ID: ''
-    PRIVATE_KEY: ''
-    KEY_ID: ''
+    SERVICE_ACCOUNT_ID: 'null'
+    PRIVATE_KEY: 'null'
+    KEY_ID: 'null'
     LLM_MAX_OUTPUT: int = 500
         
     HOST: str = "127.0.0.1"

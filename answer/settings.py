f2b89604198b542bac67560457df372c3f28d79a
Fedor Grigorev <fgrigoryev11@yandex.ru>
2025-12-02 20:49:39 +0300

    update database from bookstack

--- answer/settings.py
+++ answer/settings.py
@@ -39,7 +39,7 @@ class Settings(BaseSettings):
     retrivier_k: int = 10
     MAX_BUTTONS: int = 5
     max_length: int = 200
-    collection_name: str = 'bookstack_main'
+    collection_name: str = 'bookstack_02122025'
     warning_message: str = ' Ответ сгенерирован ИИ и может содержать неточности.'
 
 
